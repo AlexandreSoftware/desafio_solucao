692e8ebe838db0bcbb9db7f6af9659d347da0d02
xandrf <xandrf@xandrfdev.com>
2022-07-05 20:57:49 -0300

    iniciado o scaffolding do projeto, adicionando rotas

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import SideBarComponent from "./components/SideBarComponent.vue";
+  export default {
+    components: {
+      SideBarComponent
+    },
+  };
+</script>
+<template>
+  <SideBarComponent :isOpen="isOpen">
+  </SideBarComponent>
+
+</template>
+<style>
+body{
+  background-color: #121212;
+}
+</style>
---
+++ frontend/src/components/SideBarComponent.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
+<template>
+    <Button  icon="pi pi-bars" class="p-button-lg p-button-primary px-4  " @click="isOpen =true" />
+    <Sidebar v-model:visible="isOpen" position="left">
+    </Sidebar>
+</template>
